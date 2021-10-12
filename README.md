3f413997ea5c91a60344d30040919aa0e282a6df
angelomarsanz <angelomarsanz@gmail.com>
2021-10-11 23:05:47 -0400

    2021-10-11-2305

---
+++ README.md
@@ -0,0 +1,1 @@
+Este es un tema hijo de Houzez
---
+++ template-parts/dashboard/submit/form-fields/cedula_catastral_ra.php
@@ -0,0 +1,9 @@
+<div class="form-group">
+	<label for="prop_cedula_catastral_ra">Cédula catastral *</label>
+
+	<input class="form-control" required="true" name="prop_cedula_catastral_ra" id="prop_cedula_catastral_ra" value="<?php
+    if (houzez_edit_property()) {
+        houzez_field_meta('cedula_catastral');
+    }
+    ?>" placeholder="Ingrese la cédula catastral del inmueble" type="text">
+</div>
